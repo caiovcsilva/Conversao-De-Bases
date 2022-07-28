47c5b1a1247c687b9c7f0f23e2935f831797c919
Caio Victor <caiovictorsilva07@gmail.com>
2022-07-27 22:49:43 -0300

    Mudanças na funcao any2any

--- Projeto-Sérgio Galdino - UPE/Galdino.py
+++ Projeto-Sérgio Galdino - UPE/Galdino.py
@@ -11,6 +11,18 @@
 #   dec2any: (nesse você insere o número em decimal, a base original e a base para que você deseja a conversão. Nesse caso você pode converter da base 2 para qualquer outra base, por exemplo, o que não é possível no método anterior.)
 
 
+def any2dec(num_original,base_original):
+    num_original = str(num_original)
+    base_original = int(base_original)
+    dic = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    dec = 0
+    dec_temp = list(num_original)
+    dec_temp.reverse()
+    for x,i in enumerate(dec_temp):
+        dec += dic.index(i) * base_original**(x)
+    return str(dec)
+
+
 def dec2any(dec,base_final):
     base_final = int(base_final)
     dec = int(dec)
